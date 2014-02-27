a440113e3479f350a659f0e3b2512367c789061b
Cameron Jeffers <cwjeffers18@gmail.com>
2014-02-26 22:37:36 -0800

    Delete old test project

--- stealthhello/src/Test/HelloClass.java
+++
@@ -1,8 +0,0 @@
-package Test;
-
-public class HelloClass {
-	public static void main(String[] args){
-		System.out.println("hello. My name is Iago Montoya. You kill my father. Prepare to die.");
-	}
-
-}
