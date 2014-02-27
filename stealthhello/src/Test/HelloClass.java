e040c14ac80cbb58b9b07e7d75c682960f7d3279
Cameron Jeffers <cwjeffers18@gmail.com>
2014-02-26 20:41:54 -0800

    Don't mess with my dad.

--- stealthhello/src/Test/HelloClass.java
+++ stealthhello/src/Test/HelloClass.java
@@ -2,7 +2,7 @@ package Test;
 
 public class HelloClass {
 	public static void main(String[] args){
-		System.out.println("hi");
+		System.out.println("hello. My name is Iago Montoya. You kill my father. Prepare to die.");
 	}
 
 }
