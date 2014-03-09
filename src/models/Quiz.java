6351d43ead07c581270cecbe9feacbbdd4994662
jrvangogh <jrvangogh@gmail.com>
2014-03-08 16:07:28 -0800

    quiz class implemented
    Quiz class implemented based on Cameron's Question class

--- src/models/Quiz.java
+++ src/models/Quiz.java
@@ -1,11 +1,41 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Quiz superclass
+ * Stores:
+ * name
+ * description
+ * indicator - show in multiple pages or not
+ * indicator - whether quiz is timed or not
+ * date the quiz was made
+ * 
+ */
 public class Quiz extends AbstractModel{
-
-	private AbstractModel model;
 	
+	/**
+	 * Name of the MySQL table that holds the quizzes
+	 */
+	public static final String QUIZ_TABLENAME = "quizzes";
+	
+	private static final String ID_COLNAME = "id";
+	private static final String NAME_COLNAME = "name";
+	private static final String DESCRIPTION_COLNAME = "description";
+	private static final String TIMED_COLNAME = "timed";
+	private static final String MULTIPLEPAGES_COLNAME = "multiple_pages";
+	private static final String DATE_COLNAME = "date";
+	
+	
+	/**
+	 * Constructs a quiz from an AbstractModel
+	 * Assumes it is in the database
+	 * @param AbstractModel
+	 */
+	public Quiz(AbstractModel am) {
+		super(QUIZ_TABLENAME, am.getMap(), true);
+	}
 	
 	/**
 	 * Constructs a new quiz
@@ -14,50 +44,169 @@ public class Quiz extends AbstractModel{
 	 * @param givenMultiplePages whether the quiz consists of multiple pages
 	 * @param givenDateMade the date the quiz was made
 	 */
-	public Quiz(String givenName, boolean theTimed, boolean theMultiplePages, long theDateMade){
+	public Quiz(String theName, String theDescription, boolean theTimed, boolean theMultiplePages, long theDateMade){
 		super("quizzes");
+		setName(theName);
+		setDescription(theDescription);
 		setTimed(theTimed);
 		setMultiplePages(theMultiplePages);
 		setDateMade(theDateMade);
 	}
 	
-	public int getID(){
-		return (Integer) model.getValue("ID");
+	
+	// Basic data getters
+	
+	/**
+	 * @return id
+	 */
+	public Integer getID() {
+		Object id = getValue(ID_COLNAME);
+        if (id != null) return (Integer) id;
+        return null;
 	}
 	
-	public boolean isTimed(){
-		return (Boolean) model.getValue("Timed");
+	/**
+	 * @return quiz name
+	 */
+	public String getName() {
+		return (String) getValue(NAME_COLNAME);
+	}
+	
+	/**
+	 * @return quiz description
+	 */
+	public String getDescription() {
+		return (String) getValue(DESCRIPTION_COLNAME);
+	}
+	
+	/**
+	 * @return whether or not the quiz is timed
+	 */
+	public boolean isTimed() {
+		return (Boolean) getValue(TIMED_COLNAME);
 		
 	}
 	
-	public boolean hasMultiplePages(){
-		return (Boolean) model.getValue("MultiplePages");
+	/**
+	 * @return whether or not to display on multiple pages
+	 */
+	public boolean hasMultiplePages() {
+		return (Boolean) getValue(MULTIPLEPAGES_COLNAME);
 	}
 	
-	public long getDateMade(){
-		return (Long) model.getValue("Date");
+	/**
+	 * @return the date made as a long
+	 */
+	public long getDateMade() {
+		return (Long) getValue(DATE_COLNAME);
 	}
 	
-	public void setID(int id){
-		model.setValue("ID", id);
+	/**
+	 * Gets the questions associated with this quiz
+	 * Returns null if the id is not yet set
+	 * @return list of questions
+	 */
+	public List<Question> getQuestions() {
+		Integer id = getID();
+		if (id != null) {
+			return Question.findByQuizID(id);
+		}
+		return null;
 	}
 	
-	public void setTimed(boolean timed){
-		model.setValue("Timed", timed);
+	
+	// Basic data setters
+	
+	/**
+	 * @param name
+	 */
+	public void setName(String name) {
+		setValue(NAME_COLNAME, name);
 	}
 	
-	public void setMultiplePages(boolean multPages){
-		model.setValue("MultiplePages", multPages);
+	/**
+	 * @param description
+	 */
+	public void setDescription(String desc) {
+		setValue(DESCRIPTION_COLNAME, desc);
 	}
 	
-	public void setDateMade(long date){
-		model.setValue("Date", date);
+	/**
+	 * @param whether or not the quiz is timed
+	 */
+	public void setTimed(boolean timed) {
+		setValue(TIMED_COLNAME, timed);
 	}
 	
-	public void commit(){
-	//model.save();
+	/**
+	 * @param whether or not to display the quiz on multiple pages
+	 */
+	public void setMultiplePages(boolean multPages) {
+		setValue(MULTIPLEPAGES_COLNAME, multPages);
 	}
 	
-	//How to get questions
+	/**
+	 * @param the date the quiz was made
+	 */
+	public void setDateMade(long date) {
+		setValue(DATE_COLNAME, date);
+	}
 	
+	/**
+	 * Saves the quiz in the database
+	 */
+	public void commit() {
+	//save();
+	}
+	
+	// Database searching static methods
+	
+	
+	/**
+	 * Returns a quiz with the given id
+	 * Returns null if it doesn't exist
+	 * @param id
+	 * @return quiz
+	 */
+	public static Quiz findByID(int id) {
+		AbstractModel am = AbstractModel.getByID(QUIZ_TABLENAME, id);
+        if (am != null) return new Quiz(am);
+        return null;
+	}
+	
+	/**
+	 * Given a question, find the quiz associated with it
+	 * Returns null if the question does not exist
+	 * @param question id
+	 * @return quiz
+	 */
+	public static Quiz findByQuestionID(int id) {
+		Question question = (Question) AbstractModel.getByID(Question.QUESTION_TABLENAME, id);
+		if (!question.equals(null)) {
+			return ((Quiz) Quiz.findByID(question.getQuizID()));
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns all quizzes made on the certain date
+	 * Returns an empty list if none match the date
+	 * @param date
+	 * @return list of quizzes
+	 */
+	public static List<Quiz> findByDate(long date) {
+		List<AbstractModel> ams = AbstractModel.getByValue(QUIZ_TABLENAME, DATE_COLNAME, date);
+		return convertAMListToQuizzes(ams);
+	}
+	
+	/**
+	 * Converts a list of AbstractModels to a list of Quizzes
+	 */
+	private static List<Quiz> convertAMListToQuizzes(List<AbstractModel> ams) {
+		List<Quiz> quizzes = new ArrayList<Quiz>(ams.size());
+        for (AbstractModel am : ams) {
+            quizzes.add(new Quiz(am));
+        }
+        return quizzes;
+	}
 }
