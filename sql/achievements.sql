31f6971b14df510df841fc0133a81535361e3f19
Cameron Jeffers <cwjeffers18@gmail.com>
2014-03-13 16:56:34 -0700

    add achievement SQL and shell scripts

--- sql/achievements.sql
+++ sql/achievements.sql
@@ -1,10 +1,11 @@
 USE c_cs108_jvangogh;
   -- >>>>>>>>>>>> change this line so it uses your database, not mine <<<<<<<<<<<<<<<
-  
 DROP TABLE IF EXISTS achievements;
  -- remove table if it already exists and start from scratch
 
 CREATE TABLE achievements (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
     amateur_author INT(1),
     amateur_author_time BIGINT,
     prolific_author INT(1),
@@ -16,6 +17,7 @@ CREATE TABLE achievements (
     is_greatest INT(1),
     is_greatest_time BIGINT,
     practice_perfect INT(1),
-     practice_perfect_time BIGINT
-);  
-    
+    practice_perfect_time BIGINT,
+
+    PRIMARY KEY (id)
+);
