e4ff5dfdadb44461a8884fe23ee1dba343edad0e
Jed Tan <jedtan@stanford.edu>
2014-03-13 00:36:41 -0700

    Login servlet made, User password error fixed

--- src/models/test/UsersTest.java
+++ src/models/test/UsersTest.java
@@ -8,6 +8,7 @@ import models.AbstractModel;
 
 import org.junit.*;
 import models.User;
+import models.Message;
 
 public class UsersTest {
 	
@@ -22,7 +23,7 @@ public class UsersTest {
     @Test
     public void testBasicFunctionality() {
         User am = new User("ben_test", "cheese", "Ben Ulmer", true);
-      //  assertTrue(User.nameInUse("ben_test"));
+        assertTrue(User.nameInUse("ben_test"));
         am.setPicURL("testing");
         assertTrue(am.isAdministrator());
         assertTrue(am.getUserName().equals("ben_test"));
@@ -56,10 +57,26 @@ public class UsersTest {
     	User am2 = new User("ben_test_friend", "cheese", "Ninja", true);
     	List<User> admins = User.findAdministrators();
     	System.out.println(admins.size());
-    	assertTrue(admins.size() == 2);
+    	assertTrue(admins.size() == 4);
     	am1.setAdminPriveledge(false);
     	List<User> admins2 = User.findAdministrators();
-    	assertTrue(admins2.size() == 1);
+    	assertTrue(admins2.size() == 3);
+    }
+    
+    @Test
+    public void testMessages(){
+    	User am1 = new User("jed", "bleh", "Jed", false);
+    	List<Message> notes = am1.seeMessages();
+    	for(Message a:notes){
+    		System.out.println(a.getText());
+    	}
+    }
+    
+    @Test
+    public void testPassword(){
+    	User am1 = new User("james", "johnson", "Jed", false);
+    	assertTrue(User.validateLogin("james", "johnson") == true);
+    	assertTrue(User.validateLogin("james", "jones") == false);
     }
 
 }
---
+++ src/servlets/LoginServlet.java
@@ -0,0 +1,58 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.User;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		if(User.validateLogin(request.getParameter("username"), request.getParameter("password"))){
+			User newUser = User.findByUsername(request.getParameter("username"));
+			request.getSession().setAttribute("user", newUser);
+			response.sendRedirect("hello.html");
+		}
+		else{
+			RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+		    dispatcher.forward(request, response);
+		}
+		
+		//getSession().setAttribute
+		
+		
+
+
+	}
+
+}
